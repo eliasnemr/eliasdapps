fa34c27e2a6c81fabd7dd997110e5582d83086dd
Elias <eliasnemr@gmail.com>
2021-07-15 12:49:49 +0300

    MxSQL new minidapp, Wallet updated to latest version

---
+++ MxSql/minidapp/MxSql/9605.d63b2d1111512b537b9c.js
@@ -0,0 +1,1 @@
+(self.webpackChunkMxSQL=self.webpackChunkMxSQL||[]).push([[9605],{9605:(s,o,r)=>{"use strict";r.r(o),r.d(o,{ion_text:()=>e});var t=r(2322),c=r(7505),l=r(4843);const e=class{constructor(s){(0,t.r)(this,s)}render(){const s=(0,c.b)(this);return(0,t.h)(t.H,{class:(0,l.c)(this.color,{[s]:!0})},(0,t.h)("slot",null))}};e.style=":host(.ion-color){color:var(--ion-color-base)}"}}]);
